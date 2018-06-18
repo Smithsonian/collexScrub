43a24140e989d8f7958e0e61a8ab6a9dec38e6b4
Luis J. Villanueva <ljvillanueva@coquipr.com>
2018-06-18 16:15:33 -0400

    Removing packages from req for testing

--- R/misc.R
+++ R/misc.R
@@ -60,3 +60,17 @@ check_dd_dir <- function(sheet_barcode, data, gadm){
 }
 
 
+#' @export
+
+check.integer <- function(x) {
+  #from https://stackoverflow.com/q/3476782
+  x <- suppressWarnings(as.numeric(x))
+  
+  if (is.na(x)){
+    return(FALSE)
+  }else if (!(is.numeric(x))){
+    return(FALSE)
+  }else{
+    x == round(x)
+  }
+}
